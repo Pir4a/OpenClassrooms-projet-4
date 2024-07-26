220e022c4091671bb09a5056c262a45bd3aa11e3
Pi4a <piralith@outlook.fr>
2024-07-26 18:49:18 +0200

    stars, tags and logement pages advancements

---
+++ src/Components/Tags/Tag.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+import "./tag.scss"
+function Tag({ tagName }) {
+  return <div className="tag">{tagName}</div>
+}
+
+export default Tag
--- src/Pages/Logements.jsx
+++ src/Pages/Logements.jsx
@@ -2,15 +2,48 @@ import React from "react"
 import { useParams } from "react-router-dom"
 import data from "../../logements.json"
 import Carrousel from "../Components/Carrousel/Carrousel"
-
+import "./logements.scss"
+import Tag from "../Components/Tags/Tag"
+import star from "../assets/star.png"
+import graystar from "../assets/graystar.png"
 function Logements() {
   let { id } = useParams()
   let currentLogement = data.find((data) => data.id === id)
 
+  let grayStars = 5 - currentLogement.rating
+
+  const starDivs = Array.from(
+    { length: currentLogement.rating },
+    (_, index) => <img src={star} key={index} />
+  )
+
+  const grayStarDivs = Array.from({ length: grayStars }, (_, index) => (
+    <img src={graystar} key={index + 4} />
+  ))
+
   return (
-    <div>
-      <Carrousel props={currentLogement} />
-      {console.log(currentLogement.pictures)}
+    <div className="logement-container">
+      <Carrousel />
+      <div className="descriptions">
+        <div className="left-container">
+          <h3>{currentLogement.title}</h3>
+          <p>{currentLogement.location}</p>
+          <div className="tags">
+            {currentLogement.tags.map((logementTag) => (
+              <Tag key={logementTag} tagName={logementTag} />
+            ))}
+          </div>
+        </div>
+        <div className="right-container">
+          <h4>
+            {currentLogement.host.name}
+            <img src={currentLogement.host.picture} />
+          </h4>
+          <div className="stars">
+            {starDivs} {grayStarDivs}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
