8aa3d48217c1000907a769db5af666c678ab7f95
Pi4a <piralith@outlook.fr>
2024-07-26 17:28:27 +0200

    carrousel

--- src/Components/Carrousel/Carrousel.jsx
+++ src/Components/Carrousel/Carrousel.jsx
@@ -1,25 +1,43 @@
 import React, { useState } from "react"
 import "./carrousel.scss"
 import data from "../../../logements.json"
+import { useParams } from "react-router-dom"
+import vector from "../../assets/Vector.png"
+import vector2 from "../../assets/Vector2.png"
 
-function Carrousel(props) {
+function Carrousel() {
   const [imageIndex, setImageIndex] = useState(0)
+
+  let { id } = useParams()
+  let currentLogement = data.find((data) => data.id === id)
 
   function showNextImage() {
     setImageIndex((index) => {
-      if (index === imgArray.length - 1) return 0
+      if (index === currentLogement.pictures.length - 1) return 0
       return index + 1
     })
   }
 
   function showPrevImage() {
     setImageIndex((index) => {
-      if (index === 0) return imgArray.length - 1
+      if (index === 0) return currentLogement.pictures.length - 1
       return index - 1
     })
   }
 
-  return <div className="carrousel"></div>
+  return (
+    <div className="carrousel">
+      <img className="vector" src={vector} onClick={showPrevImage} />
+      <img className="vector-two" src={vector2} onClick={showNextImage} />
+      {currentLogement.pictures.map((logement) => (
+        <img
+          src={logement}
+          key={logement}
+          style={{ translate: `${-100 * imageIndex}%` }}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default Carrousel
